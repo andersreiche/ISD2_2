8bb49a368b86e440efcdf07bbaa532a0ce72fb23
andersreiche <andersreiche@gmail.com>
2018-02-09 13:06:46 +0100

    initial commit, temperature is a placeholder, GET TEMP keyword not working yet

---
+++ ISD2_2.cpp
@@ -0,0 +1,72 @@
+//============================================================================
+// Name        : ISD2_2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "ISD2_2.h"
+
+void sig_handler(int signo);
+void get_input(void);
+
+int main(int argc, char* argv[]) {
+	process_id = fork(); // Create child process
+
+	if (process_id < 0) { // Indication of fork() failure
+		printf("fork failed!\n");
+		exit(1);
+	}
+
+	if (process_id > 0) { // PARENT PROCESS. Need to kill it.
+		printf("process_id of child process %d \n", process_id);
+		exit(0);
+	}
+
+	umask(0); //unmask the file mode granting read/write/exe rights
+
+	sid = setsid(); //set new session
+	if (sid < 0) {
+		exit(1);
+	}
+
+	struct sockaddr_in servaddr;
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM = TCP socket
+	bzero(&servaddr, sizeof(servaddr)); //alternative to memset but only can set ZERO
+
+	/* Initialize socket structure */
+	servaddr.sin_family = AF_INET; //Ip V4
+	servaddr.sin_addr.s_addr = htons(INADDR_ANY); //accept any of the interfaces
+	servaddr.sin_port = htons(1955); //Port number
+
+	if (bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) { // testing if the bind goes well
+		perror("ERROR on binding");
+		exit(1);
+	}
+
+	chdir("/"); // Change the current working directory to root.
+	// Close stdin. stdout and stderr, daemonizing the process
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+        to_syslog("Started daemonized process");       
+
+	// Calls the signal handler if SIGHUP is recieved
+	if (signal(SIGHUP, sig_handler) == SIG_ERR) {
+		exit(0);
+	}
+        
+        listen(listen_fd, 10);
+	//listening to the specified ip and port and have a waiting list of 10
+	comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+
+	while (1) {
+		//Dont block context switches, let the process sleep for some time
+		sleep(1);
+		get_input();
+	}
+	fclose(fp);
+	return (0);
+        to_syslog("Daemonized process ended");
+}
---
+++ ISD2_2.h
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <signal.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+float get_temp(void);
+
+char str[100];
+int listen_fd;
+int comm_fd;
+int n = 0;
+FILE *fp = NULL;
+pid_t process_id = 0;
+pid_t sid = 0;
+
+float temp = 22.5; // Placeholder temperature reading
+
+template <typename T> string tostr(const T& t) { 
+   ostringstream os; 
+   os<<t; 
+   return os.str(); 
+} 
+
+void to_syslog(string str) {
+    openlog("Daemonisering ", LOG_PID, LOG_USER);
+    syslog(LOG_INFO, str.c_str());
+    closelog();
+}
+
+void sig_handler(int signo) {
+	openlog("Daemonisering ", LOG_PID, LOG_USER);
+	if (signo == SIGHUP)
+		syslog(LOG_INFO, "SIGHUP recieved, exiting");
+	closelog();
+	exit(0);
+}
+
+void get_input(void) {
+	bzero(str, 100); //sets str to ZERO
+	n = read(comm_fd, str, 100); //reads from the client to str and puts number of signs in to n
+	// TODO : fix this section
+	string stringstr = str;
+        to_syslog(stringstr);
+	if (stringstr == "GET TEMP") {
+            stringstr = "Temperature is " + tostr(get_temp()) + " degC.";
+            strcpy(str, stringstr.c_str());
+            write(comm_fd, str, sizeof(str)); //writes back to the client
+	}
+}
+
+float get_temp(void) {
+    // TODO Make this
+    return temp;
+}
